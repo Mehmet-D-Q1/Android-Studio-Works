1d1e2970c3f21a43d3b79826054a4e78bc7c9045
Mehmet DOGAN <mehmetdogan37@gmail.com>
2022-02-21 17:31:19 +0300

    My first medium article is sending

---
+++ medium/MediumArticles/JetpackNavigationComponent/app/src/main/java/com/example/jetpacknavigationcomponent/SecondFragment.kt
@@ -0,0 +1,31 @@
+package com.example.jetpacknavigationcomponent
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import androidx.navigation.fragment.findNavController
+
+
+class SecondFragment : Fragment() {
+    private lateinit var buttonPasswordVerify: Button
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_second, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        buttonPasswordVerify= view.findViewById(R.id.buttonPasswordVerify)!!
+        buttonPasswordVerify.setOnClickListener {
+            findNavController().navigate(R.id.action_secondFragment_to_lastFragment)
+        }
+    }
+
+}
